b95ea40a140df34172ae85e5984f72e67bccbc27
BOONKUAE BOONSUTTA <s90925@scbcorp.co.th>
2024-08-30 18:02:56 +0700

    15. Serialization & Deserialization

---
+++ src/main/java/com/bk/sec03/Lec02Serialization.java
@@ -0,0 +1,42 @@
+package com.bk.sec03;
+
+import com.bk.models.sec03.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Lec02Serialization {
+
+    private static final Logger logger = LoggerFactory.getLogger(Lec02Serialization.class);
+    private static final Path PATH = Path.of("person.out");
+
+    public static void main(String[] args) throws IOException {
+        var person = Person.newBuilder()
+                .setLastName("BK")
+                .setAge(20)
+                .setEmail("test@mail.com")
+                .setEmployed(true)
+                .setSalary(1000000.12345)
+                .setBankAccountNumber(1234566789012L)
+                .setBalance(-100000)
+                .build();
+       // logger.info(person.toString());
+
+        serialize(person);
+        logger.info("deserialize: {}",deserialize());
+        logger.info("person equals deserialized: {}",deserialize());
+        logger.info("bytes length: {}",person.toByteArray().length);
+
+    }
+
+    public static void serialize(Person person) throws IOException {
+        person.writeTo(Files.newOutputStream(PATH));
+    }
+
+    public static Person deserialize() throws IOException {
+        return Person.parseFrom(Files.newInputStream(PATH));
+    }
+}
