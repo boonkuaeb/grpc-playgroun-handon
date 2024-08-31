6c419dff0f9d5f6b50d59bf0125e5add5ba9a20e
BKHOMEDESKTOP\boonk <john.kuae@gmail.com>
2024-08-31 23:54:36 +0700

    34. Message Format Changes - Part - 2

--- src/main/java/com/bk/sec05/V2VersionCompatibility.java
+++ src/main/java/com/bk/sec05/V2VersionCompatibility.java
@@ -1,19 +1,21 @@
 package com.bk.sec05;
 
-import com.bk.models.sec05.v1.Television;
+import com.bk.models.sec05.v2.Television;
+import com.bk.models.sec05.v2.Type;
 import com.bk.sec05.parser.V1Parser;
 import com.bk.sec05.parser.V2Parser;
 import com.google.protobuf.InvalidProtocolBufferException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class V1VersionCompatibility {
-    private static final Logger log = LoggerFactory.getLogger(V1VersionCompatibility.class);
+public class V2VersionCompatibility {
+    private static final Logger log = LoggerFactory.getLogger(V2VersionCompatibility.class);
     public static void main(String[] args) throws InvalidProtocolBufferException {
 
         var tv = Television.newBuilder()
                 .setBrand("Samsung")
-                .setYear(2019)
+                .setModel(2020)
+                .setType(Type.UHD)
                 .build();
 
         V1Parser.parse(tv.toByteArray());
--- src/main/java/com/bk/sec05/parser/V2Parser.java
+++ src/main/java/com/bk/sec05/parser/V2Parser.java
@@ -1,20 +1,22 @@
 package com.bk.sec05.parser;
 
-import com.bk.models.sec05.v1.Television;
+import com.bk.models.sec05.v2.Television;
 import com.google.protobuf.InvalidProtocolBufferException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class V1Parser {
-    private static final Logger logger = LoggerFactory.getLogger(V1Parser.class);
+public class V2Parser {
+    private static final Logger logger = LoggerFactory.getLogger(V2Parser.class);
 
     public static void parse(byte[] bytes) throws InvalidProtocolBufferException
     {
         var tv = Television.parseFrom(bytes);
         logger.info("brand: {}", tv.getBrand());
-        logger.info("year: {}", tv.getYear());
+        logger.info("model: {}", tv.getModel());
+        logger.info("type: {}", tv.getType());
         logger.info(tv.getAllFields().toString());
+
 
     }
 }
