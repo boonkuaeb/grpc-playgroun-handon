1703d81f10b12d14df8a3ee328ed328223288470
BKHOMEDESKTOP\boonk <john.kuae@gmail.com>
2024-08-31 21:30:26 +0700

    25. Enum

--- src/main/java/com/bk/sec03/Lec06Map.java
+++ src/main/java/com/bk/sec03/Lec06Map.java
@@ -1,9 +1,13 @@
 package com.bk.sec03;
 
+import com.bk.models.sec03.BodyStyle;
 import com.bk.models.sec03.Car;
 import com.bk.models.sec03.Dealer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
 public class Lec06Map {
     private static final Logger log = LoggerFactory.getLogger(Lec06Map.class);
     public static void main(String[] args) {
@@ -11,22 +15,29 @@ public class Lec06Map {
                 .setMake("honda")
                 .setModel("Civic")
                 .setYear(2020)
+                .setBodyStyle(BodyStyle.COUPE)
                 .build();
         var car2 = Car.newBuilder()
                 .setMake("honda")
                 .setModel("Accord")
                 .setYear(2021)
+                .setBodyStyle(BodyStyle.SEDAN)
                 .build();
+
         var dealer = Dealer.newBuilder()
                 .putInventory(car1.getYear(),car1)
                 .putInventory(car2.getYear(),car2)
                 .build();
         log.info("dealer: {}" , dealer);
 
+        log.info("car2 getBodyStyle: {}" , car2.getBodyStyle());
+
         log.info("\n2020? : {}" , dealer.containsInventory(2020));
         log.info("2021? : {}" , dealer.containsInventory(2021));
 
         log.info("\n2020 model? : {}" , dealer.getInventoryOrThrow(2020).getModel());
         log.info("2021 model? : {}" , dealer.getInventoryOrThrow(2021));
+        log.info("2021 body style? : {}" , dealer.getInventoryOrThrow(2021).getBodyStyle());
+
     }
 }
