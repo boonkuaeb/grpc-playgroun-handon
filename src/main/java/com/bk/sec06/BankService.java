04f48361fd383bc4a9f1a315ba99327452eedfe8
BKHOMEDESKTOP\boonk <john.kuae@gmail.com>
2024-09-01 21:42:25 +0700

    57. Get All Accounts - API

--- src/main/java/com/bk/sec06/BankService.java
+++ src/main/java/com/bk/sec06/BankService.java
@@ -1,9 +1,11 @@
 package com.bk.sec06;
 
 import com.bk.models.sec06.AccountBalance;
+import com.bk.models.sec06.AllAccountsResponse;
 import com.bk.models.sec06.BalanceCheckRequest;
 import com.bk.models.sec06.BankServiceGrpc;
 import com.bk.sec06.repository.AccountRepository;
+import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,5 +24,17 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
         responseObserver.onNext(accountBalance);
         responseObserver.onCompleted();
 
+    }
+
+    @Override
+    public void getAllAccounts(Empty request, StreamObserver<AllAccountsResponse> responseObserver) {
+       var accounts = AccountRepository.allBalance()
+               .entrySet()
+               .stream()
+               .map(entry -> AccountBalance.newBuilder().setAccountNumber(entry.getKey()).setBalance(entry.getValue()).build())
+               .toList();
+       var response = AllAccountsResponse.newBuilder().addAllAccounts(accounts).build();
+       responseObserver.onNext(response);
+       responseObserver.onCompleted();
     }
 }
--- src/main/java/com/bk/sec06/repository/AccountRepository.java
+++ src/main/java/com/bk/sec06/repository/AccountRepository.java
@@ -1,5 +1,6 @@
 package com.bk.sec06.repository;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Random;
 import java.util.function.Function;
@@ -7,7 +8,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AccountRepository {
-    private static final Map<Integer,Integer> db = IntStream.rangeClosed(1,100)
+    private static final Map<Integer,Integer> db = IntStream.rangeClosed(1,10)
             .boxed()
             .collect(Collectors.toConcurrentMap(
                     Function.identity(),
@@ -17,6 +18,10 @@ public class AccountRepository {
 
     public static Integer getBalance(int accountNumber) {
         return db.get(accountNumber);
+    }
+
+    public static Map<Integer,Integer> allBalance() {
+        return Collections.unmodifiableMap(db);
     }
 
 }
