e9d4832c369b94419e5c9ebdc79c228a1d3c33fe
BKHOMEDESKTOP\boonk <john.kuae@gmail.com>
2024-09-01 20:59:29 +0700

    53. How Async Stub Works

--- src/main/java/com/bk/sec06/GrpcClient.java
+++ src/main/java/com/bk/sec06/GrpcClient.java
@@ -1,24 +1,48 @@
 package com.bk.sec06;
 
+import com.bk.models.sec06.AccountBalance;
 import com.bk.models.sec06.BalanceCheckRequest;
 import com.bk.models.sec06.BankServiceGrpc;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.ServerCallStreamObserver;
+import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+
 public class GrpcClient {
     private static final Logger log = LoggerFactory.getLogger(GrpcClient.class);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 
         var channel = ManagedChannelBuilder.forAddress("localhost", 6565)
                 .usePlaintext()
                 .build();
 
-        var stub = BankServiceGrpc.newBlockingStub(channel);
-        var balance = stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(11).build());
+        var stub = BankServiceGrpc.newStub(channel);
+        stub.getAccountBalance(BalanceCheckRequest.newBuilder().setAccountNumber(2).build(), new StreamObserver<AccountBalance>() {
+            @Override
+            public void onNext(AccountBalance accountBalance) {
+                log.info("{}", accountBalance);
+            }
 
-        log.info(balance.toString());
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                log.info("Completed");
+            }
+        });
+
+        log.info("done...");
+
+        // Waiting for onNext,onCompleted write log
+        Thread.sleep(Duration.ofSeconds(2));
+
 
     }
 }
