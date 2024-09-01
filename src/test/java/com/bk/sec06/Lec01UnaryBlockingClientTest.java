620526f5cefc463f01784e7e986aea53006b574e
BKHOMEDESKTOP\boonk <john.kuae@gmail.com>
2024-09-01 21:57:46 +0700

    58. ASSIGNMENT - Integration Test

--- src/test/java/com/bk/sec06/Lec01UnaryBlockingClientTest.java
+++ src/test/java/com/bk/sec06/Lec01UnaryBlockingClientTest.java
@@ -1,7 +1,9 @@
 package com.bk.sec06;
 
 import com.bk.models.sec06.BalanceCheckRequest;
+import com.google.protobuf.Empty;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,5 +21,14 @@ class Lec01UnaryBlockingClientTest extends AbstractTest {
         log.info("unary balance received: {}",balance);
 
         Assertions.assertEquals(100,balance.getBalance());
+    }
+
+    @Test
+    public void getAllAccounts()
+    {
+        var allAccounts = this.blockingStub.getAllAccounts(Empty.newBuilder().build());
+        log.info("unary allAccounts size: {}",allAccounts.getAccountsCount());
+        Assertions.assertEquals(10,allAccounts.getAccountsList().size());
+        Assertions.assertEquals(10, allAccounts.getAccountsCount());
     }
 }
